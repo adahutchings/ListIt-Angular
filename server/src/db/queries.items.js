ced592baca5c4430e5ec899c6336aa1ecba6dedf
Ada Hutchings <adalynn.hutchings@gmail.com>
2018-11-07 17:14:48 -0800

    changed file layout

---
+++ server/src/db/queries.items.js
@@ -0,0 +1,57 @@
+const Item = require("./models").Item;
+
+module.exports = {
+
+    addItem(newItem, callback){
+        return Item.create(newItem)
+        .then((item) => {
+            callback(null, item);
+        })
+        .catch((err) => {
+            callback(err);
+        })
+    },
+
+    getItem(id, callback){
+        return Item.findById(id)
+        .then((item) => {
+            callback(null, item);
+        })
+        .catch((err) => {
+            callback(err);
+        })
+    },
+
+    deleteItem(req, callback) {
+        return Item.findById(req.params.id)
+        .then((item) => {
+            item.destroy()
+            .then((res) => {
+                callback(null, item);
+            });
+        })
+        .catch((err) => {
+            callback(err);
+        });
+        
+    },
+
+    updateItem(req, updatedItem, callback) {
+        return Item.findById(req.params.id)
+        .then((item) => {
+            if(!item){
+                return callback("Item not found");
+            }
+
+            item.update(updatedItem, {
+                fields: Object.keys(updatedItem)
+            })
+            .then(() => {
+                callback(null, item);
+            })
+            .catch((err) => {
+                callback(err);
+            })
+        })
+    },
+}
