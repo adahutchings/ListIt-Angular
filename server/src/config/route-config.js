7fd645c2888b8e8cfed34d80a044840a7c23f199
Ada Hutchings <adalynn.hutchings@gmail.com>
2018-11-07 16:55:57 -0800

    working on node setup

---
+++ server/src/config/route-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+    init(app){
+      const staticRoutes = require("../routes/static");
+      const userRoutes = require("../routes/users");
+      const listRoutes = require("../routes/lists");
+      const itemRoutes = require("../routes/items");
+
+      app.use(staticRoutes);
+      app.use(userRoutes);
+      app.use(listRoutes);
+      app.use(itemRoutes);
+      
+    }
+  }
